f6a8196d195bc92abf5036dd12df84f2fa21729e
huw <huw@dpark.com.cn>
2022-05-23 17:53:58 +0800

    角色管理查询角色列表

--- pages/roleManage/roleManage.vue
+++ pages/roleManage/roleManage.vue
@@ -8,17 +8,15 @@
 		<view v-if="listData.length > 0">
 			<uni-group mode="card" v-for="(item, index) in listData" :key="index">
 				<view slot="title" class="card-title">
-					<text style="font-weight: bold;">
-						{{ item.roleName }}
-					</text>
+					<text style="font-weight: bold;">{{ item.roleName }}</text>
 				</view>
 				<view style="position: relative;line-height: 48rpx;padding-right: 60rpx;">
 					<view>角色编号：{{ item.roleId }}</view>
 					<view style="display: flex;">
-						<view style="width: 120rpx;">权限：</view>
-					<view style="display: flex;flex-wrap: wrap;">
-						<uni-tag :text="item" inverted type="primary" class="uni-mr-2 uni-mb-2" v-for="(item,index) in item.permissions" :key="index"></uni-tag>
-					</view>
+						<view style="width: 110rpx;">权限：</view>
+						<view style="display: flex;flex-wrap: wrap;flex:1">
+							<uni-tag :text="item" inverted type="primary" class="uni-mr-2 uni-mb-2" v-for="(item, index) in item.permissionList" :key="index"></uni-tag>
+						</view>
 					</view>
 					<view class="btn-box"><uni-icons type="more-filled" size="20" @click="item.showMenu = !item.showMenu"></uni-icons></view>
 				</view>
@@ -39,70 +37,85 @@
 </template>
 
 <script>
-	import { companyAPI, userAPI } from 'api/index.js';
-	export default {
-		data() {
-			return {
-				listData: [],
-				currentPage: 1,
-				pageSize: 10,
-				total: 0,
-				loadStatus: 'more',
-				vertical: 'bottom',
-				horizontal: 'right'
-			}
-		},
-		onLoad() {
-			this.getData()
-		},
-		onShow() {
-		},
-		//上拉加载更多
-		onReachBottom() {
-			if (this.listData.length < this.currentPage * this.pageSize) {
-				this.loadStatus = 'noMore';
-				return;
-			}
-			this.currentPage++;
-			this.getData();
-		},
-		//下拉刷新
-		onPullDownRefresh() {
-			this.currentPage = 1;
-			this.listData = [];
-			this.getData();
-		},
-		methods: {
-			getData(){
-				let temp =[
-					{roleName:'子管理员',roleId:'9894423425523',permissions:['用户子管理权限','客商初筛权限','黑名单审批权限'],showMenu:false},
-					{roleName:'黑名单审核专员',roleId:'9894423425123',permissions:['客商初筛权限','黑名单审批权限','黑名单申请权限'],showMenu:false},
-					{roleName:'信保审核专员',roleId:'943253423425523',permissions:['信保报告申请权限','信保报告列表权限','客商初筛权限'],showMenu:false},
-				]
-				this.loadStatus = 'more';
-				setTimeout(()=>{
-					this.listData = [...this.listData, ...temp];
-				},1000)
+import { companyAPI, userAPI } from 'api/index.js';
+export default {
+	data() {
+		return {
+			listData: [],
+			currentPage: 1,
+			pageSize: 10,
+			total: 0,
+			loadStatus: 'more',
+			vertical: 'bottom',
+			horizontal: 'right'
+		};
+	},
+	onLoad() {
+		this.getData();
+	},
+	onShow() {},
+	//上拉加载更多
+	onReachBottom() {
+		if (this.listData.length < this.currentPage * this.pageSize) {
+			this.loadStatus = 'noMore';
+			return;
+		}
+		this.currentPage++;
+		this.getData();
+	},
+	//下拉刷新
+	onPullDownRefresh() {
+		this.currentPage = 1;
+		this.listData = [];
+		this.getData();
+	},
+	methods: {
+		getData() {
+			let param = {
+				isFlag: 'true',
+				operate: 'admin',
+				pageIndex: this.currentPage,
+				pageSize: this.pageSize
+			};
+			this.loadStatus = 'more';
+			userAPI.getAllRole(param).then(res => {
+				this.total=res.data.totalRecords
+				let temp = res.data.allRole.content.map(item => {
+					item.permissionList = item.permission.split(',');
+					item.showMenu = false;
+					return item;
+				});
+				this.listData = [...this.listData, ...temp];
 				if (this.listData.length < this.currentPage * this.pageSize) {
 					this.loadStatus = 'noMore';
 				}
-			},
-			getAllRole(){
-				
-			},
-			fabClick() {
-				
-				uni.navigateTo({
-					url: '/pages/roleManage/create'
-				});
-			},
-			edit(item) {
-				uni.navigateTo({
-					url: `/pages/roleManage/create?roleId=${item.roleId}`
-				});
-			},
+			});
+			// let temp =[
+			// 	{roleName:'子管理员',roleId:'9894423425523',permissions:['用户子管理权限','客商初筛权限','黑名单审批权限'],showMenu:false},
+			// 	{roleName:'黑名单审核专员',roleId:'9894423425123',permissions:['客商初筛权限','黑名单审批权限','黑名单申请权限'],showMenu:false},
+			// 	{roleName:'信保审核专员',roleId:'943253423425523',permissions:['信保报告申请权限','信保报告列表权限','客商初筛权限'],showMenu:false},
+			// ]
+			// this.loadStatus = 'more';
+			// setTimeout(()=>{
+			// 	this.listData = [...this.listData, ...temp];
+			// },1000)
+			// if (this.listData.length < this.currentPage * this.pageSize) {
+			// 	this.loadStatus = 'noMore';
+			// }
+		},
+		getAllRole() {},
+		fabClick() {
+			uni.navigateTo({
+				url: '/pages/roleManage/create'
+			});
+		},
+		edit(item) {
+			uni.navigateTo({
+				url: `/pages/roleManage/create?roleId=${item.roleId}`
+			});
 		}
 	}
+};
 </script>
 
 <style scoped>
