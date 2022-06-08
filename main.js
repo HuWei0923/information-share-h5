2770433bff7e89fb17b04f7855d35e58efcf37bb
huw <huw@dpark.com.cn>
2022-06-08 17:35:32 +0800

    全局路由钩子配置

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 //引用并设置环境变量
 import '.env.js'
+import {router,RouterMount} from './router/router.js'  //路径换成自己的
+Vue.use(router)
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
@@ -11,6 +14,10 @@ App.mpType = 'app'
 const app = new Vue({
     ...App
 })
+//v1.3.5起 H5端 你应该去除原有的app.$mount();使用路由自带的渲染方式
+// #ifdef H5
+	RouterMount(app,router,'#app')
+// #endif
 app.$mount()
 // #endif
 
